506f9c734846972c5784128ad595f089ab73a1af
Abdelhak Aissat <abdelhak2478@gmail.com>
2021-12-24 11:38:22 +0100

    Implement vectorial model and the 4 functions.
    Implementation of cosinus,jaccard,dice_coef,inner product.
    I'm not sure about the design pattern or how we will use the functions
    to make our tests, maybe we will need to refactor them.

--- src/ir/main.py
+++ src/ir/main.py
@@ -249,32 +249,182 @@ def boolean_model(request:str,list_doc):
             #add to pertinent 
             pertinent_docs.append(i)
 
+        termes_in_doc.clear()
 
     return pertinent_docs
 
 
-def rsv_produit_interne():
-    """TODO:implement
+def vectorial_model(idf, list_doc, request:str) :
+    """TODO: implemnt this!
+
+    Keyword arguments:
+        idf             --  {word: {document_it_appears_in : num_occurences_in_this_doc,..},{},...}   (contains all the words!)
+        liste_doc       --  {doc_id:{word:occurence},doc_id:{word:occurence_in_doc} }  
+
+    Returns:
+        dictionnay {doc_num:{rsv_func:value_of_rsv},..}
+
     """
-    pass
+    request_list = request.split()
+   
+    req_words_vector =  {}
+    results = {}
+    for i in range(1,len(list_doc)+1): # parcourir les documents
+        # generer vecteur requete 
+        for req_term in request_list:
+            # check si mot existe dans dictionaire!
+            if  req_term in list_doc[str(i)].keys():
+                req_words_vector[req_term] = 1
+
+        # here we can call one of the functions to compute rsv
+        res = rsv_produit_interne(idf, str(i) , req_words_vector)
+
+        if res != 0:
+            tmp_dic = {"inr_prod":res}
+            results[i]= tmp_dic
+
+        req_words_vector.clear()#clear the list!
+
+    return results
 
 
-def rsv_dice_coef():
-    """TODO:implement
+
+def rsv_produit_interne(idf,doc_num:str,req_words_vector):
+    """Inner product rsv compute.
+
+    Keyword arguments:
+        idf                 --  {word: {document_it_appears_in : num_occurences_in_this_doc,..},{},...}   (contains all the words!)
+        doc_num             --  document number (or id) 
+        req_words_vector    --  {word:1} (containt only words that exists!)
+
+    Returns:
+        inner_product rsv.
     """
-    pass
+    res = 0
+    for i in idf: #parcourir tt les mots
+        if (doc_num in idf[i].keys()):
+            w_i_j = idf[i][doc_num]
+        else:
+            w_i_j  = 0
+
+        if i in req_words_vector:
+            w_i_q = req_words_vector[i]
+        else:
+            w_i_q = 0
+        res = res + w_i_j * w_i_q
+
+    return res
 
 
-def rsv_cosinus():
-    """TODO:implement
+def rsv_dice_coef(idf,doc_num:str,req_words_vector):
+    """dice coef rsv compute.
+
+    Keyword arguments:
+        idf                 --  {word: {document_it_appears_in : num_occurences_in_this_doc,..},{},...}   (contains all the words!)
+        doc_num             --  document number (or id) 
+        req_words_vector    --  {word:1} (containt only words that exists!)
+
+    Returns:
+        dice coef rsv.
     """
-    pass
+
+    res_top   = 0
+    res_w_ij2 = 0
+    res_w_iq2 = 0
+
+    for i in idf: #parcourir tt les mots
+        if (doc_num in idf[i].keys()):
+            w_i_j = idf[i][doc_num]
+        else:
+            w_i_j  = 0
+
+        if i in req_words_vector:
+            w_i_q = req_words_vector[i]
+        else:
+            w_i_q = 0
+        res_w_ij2 = res_w_ij2 + w_i_j**2
+        res_w_iq2 = res_w_iq2 + w_i_q**2
+        res_top = res_top + w_i_j * w_i_q
+
+    res_top = 2 *res_top
+
+    result = res_top / ( res_w_ij2 +res_w_iq2 )
+
+    return result
 
 
-def rsv_jaccard():
-    """TODO:implement
+def rsv_cosinus(idf,doc_num:str,req_words_vector):
+    """cosinus rsv compute.
+
+    Keyword arguments:
+        idf                 --  {word: {document_it_appears_in : num_occurences_in_this_doc,..},{},...}   (contains all the words!)
+        doc_num             --  document number (or id) 
+        req_words_vector    --  {word:1} (containt only words that exists!)
+
+    Returns:
+        cosinus rsv.
     """
-    pass
+
+    res_top   = 0
+    res_w_ij2 = 0
+    res_w_iq2 = 0
+
+    for i in idf: #parcourir tt les mots
+        if (doc_num in idf[i].keys()):
+            w_i_j = idf[i][doc_num]
+        else:
+            w_i_j  = 0
+
+        if i in req_words_vector:
+            w_i_q = req_words_vector[i]
+        else:
+            w_i_q = 0
+        res_w_ij2 = res_w_ij2 + w_i_j**2
+        res_w_iq2 = res_w_iq2 + w_i_q**2
+        res_top = res_top + w_i_j * w_i_q
+
+    
+    res_down = math.sqrt( res_w_ij2  * res_w_iq2 )
+    result = res_top / res_down
+    return result
+
+
+def rsv_jaccard(idf,doc_num:str,req_words_vector):
+    """jaccard rsv compute.
+
+    Keyword arguments:
+        idf                 --  {word: {document_it_appears_in : num_occurences_in_this_doc,..},{},...}   (contains all the words!)
+        doc_num             --  document number (or id) 
+        req_words_vector    --  {word:1} (containt only words that exists!)
+
+    Returns:
+        jaccard rsv.
+    """
+    res_top   = 0
+    res_w_ij2 = 0
+    res_w_iq2 = 0
+
+    for i in idf: #parcourir tt les mots
+        if (doc_num in idf[i].keys()):
+            w_i_j = idf[i][doc_num]
+        else:
+            w_i_j  = 0
+
+        if i in req_words_vector:
+            w_i_q = req_words_vector[i]
+        else:
+            w_i_q = 0
+
+        res_top = res_top + w_i_j * w_i_q
+
+        res_w_ij2 = res_w_ij2 + w_i_j**2
+        res_w_iq2 = res_w_iq2 + w_i_q**2
+
+    
+    res_down = res_w_ij2 + res_w_iq2 - res_top
+
+    result = res_top / res_down
+    return result
 
 
 def boolean_model0(request:str,list_doc):
@@ -311,6 +461,7 @@ def boolean_model0(request:str,list_doc):
         if eval(replace_logical_by_mathematical_operators(temp_request)):
             pertinent_docs.append(i)
 
+        termes_in_doc.clear()
     return pertinent_docs
 
 
@@ -319,7 +470,7 @@ def print_dico(dico):
     return json.dumps(dico, indent=4, sort_keys=True)
 
 
-if __name__ == '__main__':
+def main():
     doc=read_data()
     list_doc=tokenization(doc)
     idf_list=idf(list_doc)
@@ -332,4 +483,6 @@ if __name__ == '__main__':
     print('-----------------\n',boolean_model('goal or parameters',list_doc))
     
 
+if __name__ == '__main__':
+    main()
 
